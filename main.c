8524747baa8dc982c680ad08ff98935e42391f95
ThatFireBoi <gcf2007@hotmail.com>
2023-08-23 23:07:28 +0000

    Edits to code to comply with Betty styling

--- main.c
+++ main.c
@@ -18,8 +18,7 @@ int main(int argc, char **argv, char **env)
 	size_t len = 0;
 	ssize_t read;
 	int is_non_interactive = !isatty(STDIN_FILENO);
-	(void)argc;
-	(void)argv;
+	(void)argc, argv;
 
 	if (is_non_interactive)
 	{
@@ -31,24 +30,22 @@ int main(int argc, char **argv, char **env)
 		{
 			printf("$ "); /* Shell prompt */
 			read = getline(&input, &len, stdin); /* Read user input */
-
-			if (read == -1) {
+			if (read == -1)
+			{
 				printf("\n");
 				break;
 			}
 			if (input[read - 1] == '\n')
 				input[read - 1] = '\0'; /* Remove newline character */
-
-			if (strcmp(input, "exit") == 0) {
-				printf("Exiting simple shell.\n");
+			if (strcmp(input, "exit") == 0)
+			{
+				/* printf("Exiting simple shell.\n"); */
 				break;
 			}
 			if (isspace((unsigned char)input[0]))
 				continue;
-
 			if (input[0] == '\0' || input[0] == ' ')
 				continue;
-
 			execute(input, env); /* Execute the command entered by the user */
 		}
 		free(input);
