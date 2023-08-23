72bf6fb88e11362b9d8c3163078ba373376e7a18
ThatFireBoi <gcf2007@hotmail.com>
2023-08-23 23:12:10 +0000

    Edits to format to comply with Betty styling

--- is_empty_or_whitespace.c
+++ is_empty_or_whitespace.c
@@ -1,18 +1,20 @@
 #include "simple_shell.h"
 
 /**
- * is_empty_or_whitespace -Function checks if a string is empty or contains only whitespace
+ * is_empty_or_whitespace -Function checks: str is empty or contains whitespace
  *
  * @str: The input string
  *
  * Return: 1 if the string is empty or contains only whitespace, 0 otherwise
  */
 
-int is_empty_or_whitespace(const char *str) {
-    while (*str) {
-        if (!isspace((unsigned char)*str))
-            return (0);
-        str++;
-    }
-    return (1);
+int is_empty_or_whitespace(const char *str)
+{
+	while (*str)
+	{
+		if (!isspace((unsigned char)*str))
+			return (0);
+		str++;
+	}
+	return (1);
 }
