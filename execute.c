8862f4bd865dd956bae6df1915a2cc8a8de830b8
ThatFireBoi <gcf2007@hotmail.com>
2023-08-24 01:18:11 +0000

    Edits to formatting and length of functions to comply with Betty styling

--- execute.c
+++ execute.c
@@ -1,7 +1,116 @@
 #include "simple_shell.h"
 
 /**
- * execute - Function to execute a command using fork and execve
+ * tokenize_command - Tokenize the command string
+ *
+ * @command: The command to tokenize
+ *
+ * Return: An array of tokens
+ */
+
+char **tokenize_command(char *command)
+{
+	char **tokens = NULL;
+	char *token = NULL;
+	int token_count = 0;
+
+	token = strtok(command, " \n");
+	if (token == NULL)
+		return (NULL);
+
+	while (token != NULL)
+	{
+		tokens = realloc(tokens, sizeof(char *) * (token_count + 2));
+		if (tokens == NULL)
+		{
+			perror("realloc");
+			return (NULL);
+		}
+		tokens[token_count] = token;
+		token_count++;
+		token = strtok(NULL, " \n");
+	}
+	tokens[token_count] = NULL;
+	return (tokens);
+}
+
+/**
+ * execute_command - Execute a command using fork and execve
+ *
+ * @tokens: The array of tokens representing the command
+ *
+ * @env: The environment variables
+ */
+
+void execute_command(char **tokens, char **env)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("fork");
+		free(tokens);
+		return;
+	}
+	if (child_pid == 0) /* Child process */
+	{
+		if (tokens[0][0] == '/')
+		{
+			if (execve(tokens[0], tokens, env) == -1)
+			{
+				perror("execve error");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			search_and_execute(tokens, env);
+		}
+	}
+	else /* Parent process */
+	{
+		waitpid(child_pid, NULL, 0);
+		free(tokens);
+	}
+}
+
+/**
+ * search_and_execute - Search for the command in PATH directories and execute
+ *
+ * @tokens: The array of tokens representing the command
+ *
+ * @env: The environment variables
+ */
+
+void search_and_execute(char **tokens, char **env)
+{
+	char **paths = handle_paths();
+	int i;
+
+	for (i = 0; paths[i] != NULL; i++)
+	{
+		char *full_path = build_full_path(paths[i], tokens[0]);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			if (execve(full_path, tokens, env) == -1)
+			{
+				perror("execve error");
+				exit(EXIT_FAILURE);
+			}
+			free(full_path);
+			return;
+		}
+		free(full_path);
+	}
+	fprintf(stderr, "Command not found\n");
+	free(paths);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * execute - Main execution function to execute a command
  *
  * @command: The command to execute
  *
@@ -10,86 +119,10 @@
 
 void execute(char *command, char **env)
 {
-	pid_t child_pid;
-	char *token = NULL;
-	char **tokens = NULL;
-	int token_count = 0;
-	/* Tokenize the command string */
-	token = strtok(command, " \n");
-	if (token == NULL)
-		return;
+	char **tokens = tokenize_command(command);
 
-	/* Tokenize the command and store tokens in an array */
-	while (token != NULL)
+	if (tokens != NULL)
 	{
-		tokens = realloc(tokens, sizeof(char *) * (token_count + 2));
-		if (tokens == NULL)
-		{
-			perror("realloc");
-			return;
-		}
-		tokens[token_count] = token;
-		token_count++;
-		token = strtok(NULL, " \n");
-	}
-	tokens[token_count] = NULL; /* Set the last element to NULL */
-	/* Create the child process */
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("fork");
-		free(tokens); /* Free allocated memory */
-		return;
-	}
-	if (child_pid == 0) /* Child process */
-	{
-		if (tokens[0][0] == '/')
-		{
-		/* Execute the command using execve */
-			if (execve(tokens[0], tokens, env) == -1)
-			{
-				perror("execve error");
-				exit(EXIT_FAILURE); /* Exit child process with failure status */
-			}
-		}
-		else
-		{
-		/* Search for the command in the directories listed in PATH */
-		char **paths = handle_paths();
-		int i;
-
-		for (i = 0; paths[i] != NULL; i++)
-		{
-			char *full_path = (char *)malloc(strlen(paths[i]) + strlen(tokens[0]) + 2);
-
-			if (full_path == NULL)
-			{
-				perror("malloc");
-				free(paths);
-				exit(EXIT_FAILURE);
-			}
-			sprintf(full_path, "%s/%s", paths[i], tokens[0]);
-			if (access(full_path, X_OK) == 0)
-			{
-				if (execve(full_path, tokens, env) == -1)
-				{
-					perror("execve error");
-					exit(EXIT_FAILURE);
-				}
-				free(full_path);
-				return;
-			}
-			free(full_path);
-		}
-		fprintf(stderr, "Command not found\n");
-		free(paths);
-		exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{ /* Parent process */
-		/* Wait for the child process to complete */
-		waitpid(child_pid, NULL, 0);
-		free(tokens); /* Free allocated memory */
+		execute_command(tokens, env);
 	}
 }
