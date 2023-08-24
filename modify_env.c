510918fad576f0284f38cf9dc7df07e5aa2a0770
ThatFireBoi <gcf2007@hotmail.com>
2023-08-24 05:09:22 +0000

    Modification to function to modify env to have env correctl display path

--- modify_env.c
+++ modify_env.c
@@ -13,15 +13,15 @@ void modify_env(char **env, const char *variable_to_keep)
 {
 	/* Count the number of environment variables */
 	int num_vars = 0;
-	char **new_env = (char **)malloc((num_vars + 2) * sizeof(char *));
+	char **new_env = (char **)malloc((num_vars + 3) * sizeof(char *));
 	int i;
-	
+	int new_env_index = 0;
 	while (env[num_vars] != NULL)
 	{
 		num_vars++;
 	}
 
-	/* Allocate memory for the new environment array */
+	/* Create a new environment array with space for the modified PATH */
 	if (new_env == NULL)
 	{
 		perror("malloc");
@@ -33,13 +33,27 @@ void modify_env(char **env, const char *variable_to_keep)
 	{
 		if (strncmp(env[i], variable_to_keep, strlen(variable_to_keep)) == 0)
 		{
-			new_env[0] = env[i];
-			new_env[1] = NULL; /* Terminate the array */
+			new_env[new_env_index] = env[i];
+			new_env_index++;
+			new_env[new_env_index] = NULL; /* Terminate the array */
 			break;
 		}
 	}
-	/* Free the old environment array */
-	free(env);
+
+	/* Modify PATH and add standard directories */
+	new_env[new_env_index] = "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
+	new_env_index++;
+
+	/* Copy the remaining environment variables */
+	for (i = 0; env[i] != NULL; i++)
+	{
+		if (strncmp(env[i], variable_to_keep, strlen(variable_to_keep)) != 0)
+		{
+			new_env[new_env_index] = env[i];
+			new_env_index++;
+		}
+	}
+	new_env[new_env_index] = NULL;
 
 	/* Execute the command with the modified environment */
 	execute("env", new_env);
